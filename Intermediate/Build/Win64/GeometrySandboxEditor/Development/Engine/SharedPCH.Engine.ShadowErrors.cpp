2ac066bfc0ba40a02d6153f079db0998cf0b94af
Nabil Hasanov <nabil2794@hotmail.com>
2021-12-19 12:35:24 +0400

    Add project files.

---
+++ Intermediate/Build/Win64/GeometrySandboxEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29335
+#include "D:/UnrealEngine/Projects/GeometrySandbox/Intermediate/Build/Win64/GeometrySandboxEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/GeometrySandbox/MyActor.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GEOMETRYSANDBOX_MyActor_generated_h
+#error "MyActor.generated.h already included, missing '#pragma once' in MyActor.h"
+#endif
+#define GEOMETRYSANDBOX_MyActor_generated_h
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_SPARSE_DATA
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_RPC_WRAPPERS
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend struct Z_Construct_UClass_AMyActor_Statics; \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/GeometrySandbox"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor)
+
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyActor(); \
+	friend struct Z_Construct_UClass_AMyActor_Statics; \
+public: \
+	DECLARE_CLASS(AMyActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/GeometrySandbox"), NO_API) \
+	DECLARE_SERIALIZER(AMyActor)
+
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public:
+
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyActor(AMyActor&&); \
+	NO_API AMyActor(const AMyActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyActor); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMyActor)
+
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_9_PROLOG
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_SPARSE_DATA \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_RPC_WRAPPERS \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_INCLASS \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_SPARSE_DATA \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_INCLASS_NO_PURE_DECLS \
+	GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GEOMETRYSANDBOX_API UClass* StaticClass<class AMyActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GeometrySandbox_Source_GeometrySandbox_Public_MyActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
